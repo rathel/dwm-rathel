0a6850b1c4902f0e5448b3fb9813717b35966aee
rathel <rathel@gmail.com>
2022-02-16 22:08:46 -0700

    Volume Controls

--- dwm-scripts/volume.sh
+++ dwm-scripts/volume.sh
@@ -2,9 +2,14 @@
 
 LEFT="$(pactl get-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $1}')"
 RIGHT="$(pactl get-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $2}')"
+MUTE="$(pactl get-sink-mute 0 | awk '{print $2}')"
 
-if [ "$LEFT" = "$RIGHT" ]; then
-    echo "墳 ""$LEFT""%"
+if [ "$MUTE" = "yes" ]; then
+    echo "ﱝ"
 else
-    echo "墳 ""$LEFT""%"/"墳 ""$RIGHT""%"
+    if [ "$LEFT" = "$RIGHT" ]; then
+        echo "墳 ""$LEFT""%"
+    else
+        echo "墳 ""$LEFT""%"/"墳 ""$RIGHT""%"
+    fi
 fi
