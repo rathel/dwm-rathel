58a1c1ede1da60245ab284473426636d31902790
rathel <rathel@debian-asusrog>
2023-08-03 19:03:18 -0600

    updated touchpad icon

--- dwm-scripts/touchpad.sh
+++ dwm-scripts/touchpad.sh
@@ -3,7 +3,7 @@
 ENABLED="$(xinput list-props "ETPS/2 Elantech Touchpad" | awk '/Device Enabled/ {print $4}')"
 
 if [ "$ENABLED" = 0 ]; then
-    echo ""
+    echo "󰍽"
 else
-    echo ""
+    echo "󰍾"
 fi
