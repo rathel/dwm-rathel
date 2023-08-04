51dd9ba4be73a4d2522dab988e923f3789619c4b
rathel <rathel@debian-asusrog>
2023-08-03 18:54:32 -0600

    minor updates

--- dwm-scripts/volume.sh
+++ dwm-scripts/volume.sh
@@ -1,15 +1,21 @@
 #!/bin/env bash
 
-LEFT="$(pactl get-sink-volume 1 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $1}')"
-RIGHT="$(pactl get-sink-volume 1 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $2}')"
-MUTE="$(pactl get-sink-mute 1 | awk '{print $2}')"
+if [ "$(hostname)" == "debian-asusrog" ];then
+    LEFT="$(pactl get-sink-volume 0 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $1}')"
+    RIGHT="$(pactl get-sink-volume 0 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $2}')"
+    MUTE="$(pactl get-sink-mute 0 | awk '{print $2}')"
+else
+    LEFT="$(pactl get-sink-volume 1 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $1}')"
+    RIGHT="$(pactl get-sink-volume 1 | awk '{print $5,$12}' | sed 's/%//g' | awk '{print $2}')"
+    MUTE="$(pactl get-sink-mute 1 | awk '{print $2}')"
+fi
 
 if [ "$MUTE" = "yes" ]; then
     echo "󰖁"
 else
     if [ "$LEFT" = "$RIGHT" ]; then
-        echo " ""$LEFT""%"
+        echo "󰕾 ""$LEFT""%"
     else
-        echo " ""$LEFT""%"/" ""$RIGHT""%"
+        echo "󰕾 ""$LEFT""%"/"󰕾 ""$RIGHT""%"
     fi
 fi
