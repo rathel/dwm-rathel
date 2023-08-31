095a4e154faf825b3b7f73353b826a042d396315
rathel <rathel@debian-asusrog>
2023-08-31 17:46:16 -0600

    changed to alacritty

--- build.sh
+++ build.sh
@@ -10,63 +10,63 @@ DMENU=$D/dmenu-flexipatch
 DWMSCRIPTS=$D/dwm-scripts
 TABBED=$D/tabbed-flexipatch
 
-if [ "$(hostname)" == "debian-asusrog" ];then
-    cd $DWMBLOCKS
-    cp -v blocks.def.h-asusrog blocks.def.h
+if [ "$(hostname)" == "debian-asusrog" ]; then
+	cd $DWMBLOCKS
+	cp -vf blocks.def.h-asusrog blocks.def.h
 else
-    cd $DWMBLOCKS
-    cp -v blocks.def.h-default blocks.def.h
+	cd $DWMBLOCKS
+	cp -vf blocks.def.h-default blocks.def.h
 fi
 
-DIRECTORIES=( "$DWM"
-              "$DWMBLOCKS"
-              "$DMENU"
-              "$SLOCK"
-              "$ST" 
-              "$TABBED" )
+DIRECTORIES=("$DWM"
+	"$DWMBLOCKS"
+	"$DMENU"
+	"$SLOCK"
+	"$ST"
+	"$TABBED")
 
-FILESTOREMOVE=( "blocks.h"
-		"patches.h"
-		"config.h" )
+FILESTOREMOVE=("blocks.h"
+	"patches.h"
+	"config.h")
 
-if ! [ "$(command -v unzip)" ];then
-    echo "Needs unzip"
-    exit 1
+if ! [ "$(command -v unzip)" ]; then
+	echo "Needs unzip"
+	exit 1
 fi
-if ! [ "$(find /usr/include/X11 -iname "xft.h")" ];then
-    echo "Needs libXft"
-    exit 1
+if ! [ "$(find /usr/include/X11 -iname "xft.h")" ]; then
+	echo "Needs libXft"
+	exit 1
 fi
-if ! [ "$(find /usr/include/X11 -iname "xinerama.h")" ];then
-    echo "Needs libXinerama"
-    exit 1
+if ! [ "$(find /usr/include/X11 -iname "xinerama.h")" ]; then
+	echo "Needs libXinerama"
+	exit 1
 fi
-if ! [ "$(find /usr/include/X11 -iname "xrandr.h")" ];then
-    echo "Needs libXrandr"
-    exit 1
-fi
-
-if ! [ "$(command -v kitty)" ];then
-    echo "Needs Kitty Terminal"
-    exit 1
-fi
-if ! [ "$(command -v dunst)" ];then
-    echo "Needs dunst"
-    exit 1
+if ! [ "$(find /usr/include/X11 -iname "xrandr.h")" ]; then
+	echo "Needs libXrandr"
+	exit 1
 fi
 
-BUILD(){
-        echo "Running make..."
-        echo ""
-        make
-        echo ""
-        echo "Running make clean install..."
-        echo ""
-        sudo make clean install
+if ! [ "$(command -v kitty)" ]; then
+	echo "Needs Kitty Terminal"
+	exit 1
+fi
+if ! [ "$(command -v dunst)" ]; then
+	echo "Needs dunst"
+	exit 1
+fi
+
+BUILD() {
+	echo "Running make..."
+	echo ""
+	make
+	echo ""
+	echo "Running make clean install..."
+	echo ""
+	sudo make clean install
 }
-REMOVEFILES(){
+REMOVEFILES() {
 	set +eou pipefail
-	for files in "${FILESTOREMOVE[@]}";do
+	for files in "${FILESTOREMOVE[@]}"; do
 		rm -v $files
 	done
 	set -eou pipefail
@@ -80,54 +80,54 @@ REMOVEFILES(){
 #        done
 #}
 
-for bdir in "${DIRECTORIES[@]}";do
-    cd $bdir
-    REMOVEFILES
-#    REPLACE
-    BUILD
+for bdir in "${DIRECTORIES[@]}"; do
+	cd $bdir
+	REMOVEFILES
+	#    REPLACE
+	BUILD
 done
 
-if [ ! -d $HOME/.local/share/dwm ];then
-    echo ""
-    echo "Making Script Folder..."
-    mkdir -p $HOME/.local/share/dwm
-    echo ""
-    echo "Copying Scripts..."
-    cp -vf $DWMSCRIPTS/* $HOME/.local/share/dwm/
+if [ ! -d $HOME/.local/share/dwm ]; then
+	echo ""
+	echo "Making Script Folder..."
+	mkdir -p $HOME/.local/share/dwm
+	echo ""
+	echo "Copying Scripts..."
+	cp -vf $DWMSCRIPTS/* $HOME/.local/share/dwm/
 else
-    echo ""
-    echo "Copying Scripts..."
-    cp -v $DWMSCRIPTS/* $HOME/.local/share/dwm/
+	echo ""
+	echo "Copying Scripts..."
+	cp -v $DWMSCRIPTS/* $HOME/.local/share/dwm/
 fi
 
-if [ ! -d /usr/share/xsessions ];then
-    sudo mkdir -p /usr/share/xsessions
-    echo "Copying desktop file..."
-    sudo cp -vf $D/dwm.desktop /usr/share/xsessions/dwm.desktop
-elif [ ! -f /usr/share/xsessions/dwm.desktop ];then
-    sudo cp -vf $D/dwm.desktop /usr/share/xsessions/dwm.desktop
+if [ ! -d /usr/share/xsessions ]; then
+	sudo mkdir -p /usr/share/xsessions
+	echo "Copying desktop file..."
+	sudo cp -vf $D/dwm.desktop /usr/share/xsessions/dwm.desktop
+elif [ ! -f /usr/share/xsessions/dwm.desktop ]; then
+	sudo cp -vf $D/dwm.desktop /usr/share/xsessions/dwm.desktop
 fi
 
-if [ ! -d $HOME/.config/kitty ];then
-    echo "Making Kitty Folder..."
-    mkdir -p $HOME/.config/kitty
-    echo "Copying Kitty config..."
-    cp -vf $D/kitty.conf $HOME/.config/kitty/
+if [ ! -d $HOME/.config/kitty ]; then
+	echo "Making Kitty Folder..."
+	mkdir -p $HOME/.config/kitty
+	echo "Copying Kitty config..."
+	cp -vf $D/kitty.conf $HOME/.config/kitty/
 else
-    echo "Copying Kitty config..."
-    cp -vf $D/kitty.conf $HOME/.config/kitty/
+	echo "Copying Kitty config..."
+	cp -vf $D/kitty.conf $HOME/.config/kitty/
 fi
 
-if [[ $(fc-list) != *Jet* ]];then
-    wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
-    unzip /tmp/JetBrainsMono.zip -d $HOME/.fonts
-    fc-cache
+if [[ $(fc-list) != *Jet* ]]; then
+	wget -c https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -O /tmp/JetBrainsMono.zip
+	unzip /tmp/JetBrainsMono.zip -d $HOME/.fonts
+	fc-cache
 fi
 
 echo ""
 
 cd $DWMBLOCKS
-cp  blocks.def.h-default blocks.def.h
+cp blocks.def.h-default blocks.def.h
 
 echo "Done..."
 exit 0
--- dwm-scripts/autostart.sh
+++ dwm-scripts/autostart.sh
@@ -10,22 +10,28 @@
 
 ### HEADER ###
 
-LOCKFILE="/var/lock/`basename $0`"
+LOCKFILE="/var/lock/$(basename $0)"
 LOCKFD=99
 
 # PRIVATE
-_lock()             { flock -$1 $LOCKFD; }
-_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
-_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+_lock() { flock -$1 $LOCKFD; }
+_no_more_locking() {
+	_lock u
+	_lock xn && rm -f $LOCKFILE
+}
+_prepare_locking() {
+	eval "exec $LOCKFD>\"$LOCKFILE\""
+	trap _no_more_locking EXIT
+}
 
 # ON START
 _prepare_locking
 
 # PUBLIC
-exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
-exlock()            { _lock x; }   # obtain an exclusive lock
-shlock()            { _lock s; }   # obtain a shared lock
-unlock()            { _lock u; }   # drop a lock
+exlock_now() { _lock xn; } # obtain an exclusive lock immediately or fail
+exlock() { _lock x; }      # obtain an exclusive lock
+shlock() { _lock s; }      # obtain a shared lock
+unlock() { _lock u; }      # drop a lock
 
 ### BEGIN OF SCRIPT ###
 $HOME/.screenlayout/layout.sh &
@@ -35,7 +41,8 @@ dwmblocks &
 #/home/rathel/.local/share/dwm/statusbar.sh &
 #wal -R &
 #firefox-developer-edition &
-kitty &
+#kitty &
+alacritty &
 #st &
 numlockx &
 #compton &
